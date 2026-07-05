0e5185b9d755f1beaf3e7d24e886385d952d9092
TakumaNakamura <91472318+tkmJackie@users.noreply.github.com>
2026-07-05 21:46:12 +0900

    Add files via upload

---
+++ vendor/mathjax-config.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true,
+    packages: {"[+]": ["ams", "boldsymbol", "mathtools"]}
+  },
+  options: {
+    skipHtmlTags: ["script", "noscript", "style", "textarea", "pre", "code"]
+  },
+  startup: {
+    typeset: false
+  },
+  svg: {
+    fontCache: "global"
+  }
+};
